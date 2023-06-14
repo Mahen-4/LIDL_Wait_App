5950c546e0b4618b475453bc20e4b835c426f749
Mahen-4 <mahen.ath@outlook.com>
2023-06-14 19:20:56 +0200

    numpad num input [DONE]

--- react-client/src/components/Game.tsx
+++ react-client/src/components/Game.tsx
@@ -1,38 +1,68 @@
 
 import * as React from 'react';
-import {FC} from 'react'
+import {FC, useState} from 'react'
 import axios from 'axios'
 export const Game:FC =  () => {
 
-    axios.get("http://127.0.0.1:5000/game_fruit&Legume")
-    .then((response) => {
-        console.log(response.data.product[0])
-    })
+    // //get all product from backend
+    // React.useEffect(()=>{
+    //     axios.get("http://127.0.0.1:5000/game_fruit&Legume")
+    //     .then((response) => {
+    //         console.log(response.data.product[0])
+    //     })
+    // },[])
+
+    const [inputNum, setInputNum] = useState(0);
+    const [calculatedNum, setCalculatedNum] = useState(0);
+    const [operator, setoperator] = useState(' ');
+    const [isDecimal, setIsDecimal] = useState(false);
+    const [decimalCount, setDecimalCount] = useState(1);
+    const [monitor, setMonitor] = useState(0)
+    
+    React.useEffect(()=>{
+        setMonitor(inputNum);
+    },[inputNum])
+    
+
+    // HANDLING NUM INPUT
+    const takeInputNum = (num: number) =>{
+        if(isDecimal){
+            num = num/Math.pow(10,decimalCount)
+            setDecimalCount(decimalCount+1)
+            setInputNum(parseFloat((inputNum+num).toFixed(decimalCount)))
+        }
+        else{
+            setInputNum(inputNum*10+num)
+        }
+        
+    }
+    
   return (
     <div>
         <section className='monitor'>
-            <p className='output'>0</p>
+            <p className='output'>{monitor}</p>
         </section>
         <section className='keyboard'>
             <div className='keyboard-row'>
-                <button className='one-block'>1</button>
-                <button className='one-block'>2</button>
-                <button className='one-block'>3</button>
+                <button className='one-block' value="1" onClick={() => {takeInputNum(1)}}>1</button>
+                <button className='one-block' value="2" onClick={() => {takeInputNum(2)}}>2</button>
+                <button className='one-block' value="3" onClick={() => {takeInputNum(3)}}>3</button>
             </div>
             <div className='keyboard-row'>
-                <button className='one-block'>4</button>
-                <button className='one-block'>5</button>
-                <button className='one-block'>6</button>
+                <button className='one-block' value="4" onClick={() => {takeInputNum(4)}}>4</button>
+                <button className='one-block' value="5" onClick={() => {takeInputNum(5)}}>5</button>
+                <button className='one-block' value="6" onClick={() => {takeInputNum(6)}}>6</button>
             </div>
             <div className='keyboard-row'>
-                <button className='one-block'>7</button>
-                <button className='one-block'>8</button>
-                <button className='one-block'>9</button>
+                <button className='one-block' value="7" onClick={() => {takeInputNum(7)}}>7</button>
+                <button className='one-block' value="8" onClick={() => {takeInputNum(8)}}>8</button>
+                <button className='one-block' value="9" onClick={() => {takeInputNum(9)}}>9</button>
             </div>
             <div className='keyboard-row'>
-                <button className='one-block'>0</button>
-                <button className='one-block'>.</button>
-                <button className='one-block'>=</button>
+                <button className='one-block' value="0" onClick={() => {takeInputNum(0)}}>0</button>
+                <button className='one-block' value="." onClick={() => {setIsDecimal(true)}}>.</button>
+                <button className='one-block' value="Supp">Supp</button>
+                <button className='one-block' value="=" >=</button>
             </div>
         </section>
     </div>
